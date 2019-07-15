a8a5be57117733801a8c1f1b327486ffb90194c9
Graham Fairweather <xotic750@gmail.com>
2019-07-15 17:49:45 +0200

    :bookmark: v2.0.0
    Auto build

--- __tests__/get-own-enumerable-property-symbols-x.test.js
+++ __tests__/get-own-enumerable-property-symbols-x.test.js
@@ -1,48 +1,30 @@
-let getOEPS;
+import getOEPS from '../src/get-own-enumerable-property-symbols-x';
 
-if (typeof module === 'object' && module.exports) {
-  require('es5-shim');
-  require('es5-shim/es5-sham');
-
-  if (typeof JSON === 'undefined') {
-    JSON = {};
-  }
-
-  require('json3').runInContext(null, JSON);
-  require('es6-shim');
-  const es7 = require('es7-shim');
-  Object.keys(es7).forEach(function(key) {
-    const obj = es7[key];
-
-    if (typeof obj.shim === 'function') {
-      obj.shim();
-    }
-  });
-  getOEPS = require('../../index.js');
-} else {
-  getOEPS = returnExports;
-}
-
+/* eslint-disable-next-line compat/compat */
 const hasSymbolSupport = typeof Symbol === 'function' && typeof Symbol('') === 'symbol';
 
 let definedNonEnumerable;
 let definedEnumerable;
 
 if (hasSymbolSupport) {
+  /* eslint-disable-next-line compat/compat */
   let testObj = Object.defineProperty({}, Symbol('first'), {
     enumerable: false,
     value: 'first',
   });
 
+  /* eslint-disable-next-line compat/compat */
   const nonEnum = Object.getOwnPropertySymbols(testObj);
-  // eslint-disable-next-line no-prototype-builtins
+  /* eslint-disable-next-line no-prototype-builtins */
   definedNonEnumerable = nonEnum.length === 1 && Object.propertyIsEnumerable(testObj, nonEnum) === false;
 
+  /* eslint-disable-next-line compat/compat */
   testObj = Object.defineProperty({}, Symbol('second'), {
     enumerable: true,
     value: 'second',
   });
 
+  /* eslint-disable-next-line compat/compat */
   definedEnumerable = Object.getOwnPropertySymbols(testObj).length === 1;
 }
 
@@ -51,24 +33,28 @@ const ifEnumerableSymbols = definedEnumerable ? it : xit;
 
 describe('getOEPS', function() {
   it('is a function', function() {
+    expect.assertions(1);
     expect(typeof getOEPS).toBe('function');
   });
 
   it('should throw when target is null or undefined', function() {
+    expect.assertions(3);
     expect(function() {
       getOEPS();
-    }).toThrow();
+    }).toThrowErrorMatchingSnapshot();
 
     expect(function() {
+      /* eslint-disable-next-line no-void */
       getOEPS(void 0);
-    }).toThrow();
+    }).toThrowErrorMatchingSnapshot();
 
     expect(function() {
       getOEPS(null);
-    }).toThrow();
+    }).toThrowErrorMatchingSnapshot();
   });
 
   it('should work with other primitives', function() {
+    expect.assertions(1);
     const values = [1, true, 'ac'];
 
     const expected = values.map(function() {
@@ -83,15 +69,19 @@ describe('getOEPS', function() {
   });
 
   it('should return empty array', function() {
+    expect.assertions(1);
     expect(getOEPS({bar: 1, foo: 2})).toStrictEqual([]);
   });
 
   ifNonEnumerableSymbols('should return enumerable own property symbols', function() {
+    expect.assertions(1);
+    /* eslint-disable-next-line compat/compat */
     const obj = Object.defineProperty({bar: 1, foo: 2}, Symbol('first'), {
       enumerable: false,
       value: 'first',
     });
 
+    /* eslint-disable-next-line compat/compat */
     const symbol = Symbol('second');
     Object.defineProperty(obj, symbol, {
       enumerable: true,
@@ -102,6 +92,8 @@ describe('getOEPS', function() {
   });
 
   ifEnumerableSymbols('should return enumerable own property symbols', function() {
+    expect.assertions(1);
+    /* eslint-disable-next-line compat/compat */
     const symbol = Symbol('first');
     const obj = Object.defineProperty({bar: 1, foo: 2}, symbol, {
       enumerable: true,
