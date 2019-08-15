5e3e8bbe2acff8b1733e08ac37826ae4b796bdd0
Graham Fairweather <xotic750@gmail.com>
2019-08-15 20:24:47 +0200

    Maintenance

--- dist/get-own-enumerable-property-symbols-x.js
+++ dist/get-own-enumerable-property-symbols-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-14T15:20:15.405Z",
+  "date": "2019-08-15T18:24:41.132Z",
   "describe": "",
   "description": "Like Object.getOwnPropertySymbols but gets only enumerable properties.",
   "file": "get-own-enumerable-property-symbols-x.js",
